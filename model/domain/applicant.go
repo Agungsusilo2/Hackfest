f09a557f729d4993c9df6a783fb4264d1a7e736d
Agung <agungsusilo683@gmail.com>
2024-01-11 01:11:04 +0700

    adding files

---
+++ model/domain/applicant.go
@@ -0,0 +1,9 @@
+package domain
+
+type Category struct {
+	Id                    int32
+	EventName             string
+	Date                  string
+	EventVenues           string
+	RequeirementMaterials string
+}
